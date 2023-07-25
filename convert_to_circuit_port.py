2c9706276bc17c8afbcd30afe4df0a763f5b9757
cairaborchers <100237683+cairaborchers@users.noreply.github.com>
2023-07-25 08:25:17 -0400

    Add files via upload

---
+++ convert_to_circuit_port.py
@@ -0,0 +1,10 @@
+# ----------------------------------------------
+# Script Recorded by Ansys Electronics Desktop Version 2023.1.0
+# 16:39:00  Jul 24, 2023
+# ----------------------------------------------
+
+import ScriptEnv
+
+ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
+oDesktop.RestoreWindow()
+oProject = oDesktop.NewProject()
